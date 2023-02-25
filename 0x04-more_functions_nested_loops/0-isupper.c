feb06f1c1777c4f18cf834a4ac28ce1bbd37511a
valley769 <ubanidavid769@gmail.com>
2023-02-24 18:17:30 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,9 +7,12 @@
   */
 int _isupper(int x)
 {
-        if (x >= 65 && x <= 90)
-        {
-        return(1);
-        }
-        return(0);
+    if (x >= 65 && x <= 90)  /* ASCII values for A and Z */
+    {
+        return 1;  /* c is an uppercase character */
+    }
+    else
+    {
+        return 0;
+	}
 }
