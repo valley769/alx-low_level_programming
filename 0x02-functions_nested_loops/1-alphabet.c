240a3c9e58b5fd3aafe8b58a25996e96b4b9eab2
valley769 <ubanidavid769@gmail.com>
2023-02-25 14:34:10 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Main: print the alphabetic
+ * Return: always succes
+ */
+
+int main(void)
+
+{
+	char c;
+	
+	for (c = 'a'; c <= '2'; c++)
+	_putchar(c);
+	_putchar('\n');
+	return(0);
+}
